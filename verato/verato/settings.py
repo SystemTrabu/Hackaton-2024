65e204f21b092961a14e6ca1b843c891b6e31b11
Arturo Torres <128834206+SystemTrabu@users.noreply.github.com>
2024-03-15 11:59:48 -0600

    mongodb

--- verato/verato/settings.py
+++ verato/verato/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'verato.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'ussers',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': '189.176.237.105', #esto es modificable
+            'port': 32
+        }
     }
 }
 
